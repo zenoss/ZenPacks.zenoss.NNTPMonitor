826dd2a3c02302a1567811f46fe3df5af7fd1b95
cgibbons <cgibbons@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-22 03:36:49 +0000

    * Fixes # 3005 : new ZenPack for monitoring NNTP server response time
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.NNTPMonitor@22538 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/NNTPMonitor/datasources/NNTPMonitorDataSource.py
@@ -0,0 +1,110 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2008, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+
+import Products.ZenModel.RRDDataSource as RRDDataSource
+from Products.ZenModel.ZenPackPersistence import ZenPackPersistence
+from AccessControl import ClassSecurityInfo, Permissions
+from Products.ZenUtils.ZenTales import talesCompile, getEngine
+
+class NNTPMonitorDataSource(ZenPackPersistence, RRDDataSource.RRDDataSource):
+    
+    NNTP_MONITOR = 'NNTPMonitor'
+    
+    sourcetypes = (NNTP_MONITOR,)
+    sourcetype = NNTP_MONITOR
+
+    eventClass = '/Status/NNTP'
+        
+    nntpServer = '${dev/id}'
+    useSSL = False
+    port = 119
+    timeout = 60
+
+    _properties = RRDDataSource.RRDDataSource._properties + (
+        {'id':'nntpServer', 'type':'string', 'mode':'w'},
+        {'id':'useSSL', 'type':'boolean', 'mode':'w'},
+        {'id':'port', 'type':'int', 'mode':'w'},
+        {'id':'timeout', 'type':'int', 'mode':'w'},
+        )
+        
+    _relations = RRDDataSource.RRDDataSource._relations + (
+        )
+
+
+    factory_type_information = ( 
+    { 
+        'immediate_view' : 'editNNTPMonitorDataSource',
+        'actions'        :
+        ( 
+            { 'id'            : 'edit',
+              'name'          : 'Data Source',
+              'action'        : 'editNNTPMonitorDataSource',
+              'permissions'   : ( Permissions.view, ),
+            },
+        )
+    },
+    )
+
+    security = ClassSecurityInfo()
+
+
+    def __init__(self, id, title=None, buildRelations=True):
+        RRDDataSource.RRDDataSource.__init__(self, id, title, buildRelations)
+
+
+    def getDescription(self):
+        if self.sourcetype == self.NNTP_MONITOR:
+            return self.nntpServer + self.port
+        return RRDDataSource.RRDDataSource.getDescription(self)
+
+
+    def useZenCommand(self):
+        return True
+
+
+    def getCommand(self, context):
+        parts = self.useSSL and ['check_nntps'] or ['check_nntp']
+        if self.nntpServer:
+            parts.append('-H %s' % self.nntpServer)
+        if self.port:
+            parts.append('-p %d' % self.port)
+        if self.timeout:
+            parts.append('-t %d' % self.timeout)
+
+        cmd = ' '.join(parts)
+        cmd = '$ZENHOME/libexec/' + \
+                    RRDDataSource.RRDDataSource.getCommand(self, context, cmd)
+        return cmd
+
+
+    def checkCommandPrefix(self, context, cmd):
+        return cmd
+
+
+    def addDataPoints(self):
+        if not hasattr(self.datapoints, 'time'):
+            self.manage_addRRDDataPoint('time')
+
+
+    def zmanage_editProperties(self, REQUEST=None):
+        '''validation, etc'''
+        if REQUEST:
+            # ensure default datapoint didn't go away
+            self.addDataPoints()
+            # and eventClass
+            if not REQUEST.form.get('eventClass', None):
+                REQUEST.form['eventClass'] = self.__class__.eventClass
+        return RRDDataSource.RRDDataSource.zmanage_editProperties(self, REQUEST)
+
+
